8c9092d06a9020f068807728366b7b7410caaaff
Ruri <akizuki.ruri@gmail.com>
2020-07-17 16:59:11 +0900

    add longitudinal aberration to wavefront function

--- SimLight/__init__.py
+++ SimLight/__init__.py
@@ -18,6 +18,7 @@ from .propagation import propagation, near_field_propagation
 from .calc import (phase, intensity, aberration, sidel_aberration,
                    delta_wavefront)
 from .aperture import circle_aperture
+from .utils import longitude_to_wavefront
 from . import plottools, zernike
 
 sl_version = __version__
--- SimLight/utils.py
+++ SimLight/utils.py
@@ -7,8 +7,11 @@ Created on May 22, 2020
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 import SimLight as sl
+from .calc import phase
 
 
 def pv(phase, mask=False):
@@ -118,23 +121,81 @@ def zernike_to_sidel(zernike_coefficients):
     return sidel_coefficients
 
 
-def longitude_to_wavefront(delta_W, h, wavelength=0.550):
+def longitude_to_wavefront(lens, longitude, wavelength=0.550):
     """
     Covert the longitudinal spherical aberration function to sidel
     coefficients.
 
     Args:
+        lens: tuple
+            The lens with spherical aberration.
+        longitude: list
+            The longitudinal spherical aberration funciton of the lens.
+        wavelength: float
+            The wavelenth of used light.
     Returns:
+        field: tuple
+            The light field after passing through the lens.
+    Yeild:
+        Two figures of longitudinal aberration and wavefront.
     """
-    delta_W /= wavelength * 1e-3 / (2 * np.pi)
+    D = lens.D
+    f = lens.f
+    mask_r = 1
+    N = len(longitude)
+    center = int(N / 2)
+    k = 2 * np.pi / wavelength
+    if (np.abs(np.max(longitude[center, center:])) >
+            np.abs(np.min(longitude[center, center:]))):
+        l_max_value = np.abs(np.max(longitude[center, center:]) * 2.5)
+    else:
+        l_max_value = np.abs(np.min(longitude[center, center:]) * 2.5)
 
-    N = len(delta_W)
-    x = np.linspace(-1, 1, 2 * N)
+    x = np.linspace(-D / 2, D / 2, N)
     X, Y = np.meshgrid(x, x)
-    rho = np.sqrt(X**2 + Y**2)
-    theta = np.arctan2(Y, X)
+    rho = np.sqrt(X**2 + Y**2) / (D / 2)
 
-    sidel = sl.zernike.SidelCoefficients()
-    sidel.coefficients[5][0] = delta_W / 4 / rho**3
+    delta_w = longitude * D / -f**2
+    delta_W = delta_w / (wavelength * 1e-3 / (2 * np.pi))
+    W = delta_W * rho / 4
+    varphi = W * k
 
-    return sidel
+    field = sl.PlaneWave(wavelength, D, N)
+    field.complex_amp *= np.exp(-1j * varphi)
+    phase_ = phase(field, unwrap=True)
+    phase_ = wavelength * phase_ / (2 * np.pi)
+    _, _, norm_radius = circle_aperature(phase_, mask_r)
+    w_max_value = np.max(phase_[norm_radius <= mask_r])
+    w_min_value = np.min(phase_[norm_radius <= mask_r])
+    PV = 'P-V: ' + str(round(pv(phase_, mask=True), 3)) + ' λ'
+    RMS = 'RMS: ' + str(round(rms(phase_, mask=True), 3)) + ' λ'
+
+    fig = plt.figure(figsize=(9, 6))
+    grid = plt.GridSpec(8, 8, wspace=0.5, hspace=0.5)
+    ax1 = fig.add_subplot(grid[0:6, 0:2])
+    ax2 = fig.add_subplot(grid[0:6, 3:8])
+
+    im1 = ax1.plot(longitude[center, center:], x[center:])
+    ax1.spines['top'].set_color('none')
+    ax1.spines['bottom'].set_position(('data', 0))
+    ax1.spines['left'].set_position(('data', 0))
+    ax1.spines['right'].set_color('none')
+    ax1.set_xlim((-l_max_value, l_max_value))
+    ax1.set_xlabel('Longitudinal aberration [mm]')
+    ax1.set_ylabel('Size [mm]', rotation=0, position=(0, 1.01), labelpad=-20)
+
+    extent = [-D / 2, D / 2, -D / 2, D / 2]
+    im2 = ax2.imshow(phase_, cmap='rainbow', extent=extent,
+                     vmin=w_min_value, vmax=w_max_value)
+    mask = patches.Circle([0, 0], D * mask_r / 2, fc='none', ec='none',)
+    ax2.add_patch(mask)
+    im2.set_clip_path(mask)
+    ax2.text(0.05, 0.95, PV, fontsize=12, horizontalalignment='left',
+             transform=ax2.transAxes)
+    ax2.text(0.05, 0.90, RMS, fontsize=12, horizontalalignment='left',
+             transform=ax2.transAxes)
+    plt.colorbar(im2, ax=ax2)
+
+    plt.show()
+
+    return field
